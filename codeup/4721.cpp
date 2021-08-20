e44094fbd171b55804aa4bf56813a0df100c69dc
JUSEOUNGHYUN <succeed3256@naver.com>
2021-08-20 20:21:37 +0900

    codeup 4721 투표 보류.file

---
+++ codeup/4721.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int test;
+    cin>>test;
+
+    int a,b,c;
+    
+    int sum1=0, sum2=0, sum3=0;
+
+    for(int i=0; i<test; i++){
+        cin>>a>>b>>c;
+        
+    }
+}
+
+//보류
