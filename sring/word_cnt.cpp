03fa559513afe8cdd227433101f7186499d88bb7
JUSEOUNGHYUN <succeed3256@naver.com>
2021-09-20 22:40:55 +0900

    단어 개수 새기.file

---
+++ sring/word_cnt.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    string str;
+    getline(cin, str);
+
+    int cnt=0;
+
+    for(int i=0; i<str.length(); i++){
+        if(str[i]==' '){
+            cnt++;
+        }
+    }
+    cout<<cnt+1;
+}
