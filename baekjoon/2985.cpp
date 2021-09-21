95b9e5f66526393a56e33e1666a741046a884634
JUSEOUNGHYUN <succeed3256@naver.com>
2021-09-21 16:46:27 +0900

    세수가 주어졌을 때, 적어준 등식을 구하는 코드.file

---
+++ baekjoon/2985.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double a,b,c;
+    cin>>a>>b>>c;
+
+    if(a+b==c){
+        cout<<a<<"+"<<b<<"="<<c;
+    }
+    else if(a==b+c){
+        cout<<a<<"="<<b<<"+"<<c;
+    }
+    else if(a-b==c){
+        cout<<a<<"-"<<b<<"="<<c;
+    }
+    else if(a==b-c){
+        cout<<a<<"="<<b<<"-"<<c;
+    }
+    else if(a*b==c){
+        cout<<a<<"*"<<b<<"="<<c;
+    }
+    else if(a==b*c){
+        cout<<a<<"="<<b<<"*"<<c;
+    }
+    else if(a/b==c){
+        cout<<a<<"/"<<b<<"="<<c;
+    }
+    else if(a==b/c){
+        cout<<a<<"="<<b<<"/"<<c;
+    }
+}
